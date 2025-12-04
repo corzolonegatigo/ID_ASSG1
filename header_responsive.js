f450a41d708739e116cb04ae76423f438857d9aa
corzolonegatigo <zlee26369@gmail.com>
2025-12-04 09:27:08 +0800

    header background responsiveness

---
+++ header_responsive.js
@@ -0,0 +1,33 @@
+/* Taken from 'https://developer.mozilla.org/en-US/docs/Web/API/Document/scroll_event' */
+
+let lastKnownScrollPosition = 0;
+let ticking = false;
+let headerTag = document.getElementById('header')
+
+function setHeaderBackground(scrollPos) {
+    console.log(scrollPos)
+    if (scrollPos > 800) {
+        headerTag.style.backgroundColor = "black";
+        headerTag.style.borderBottom = "2px";
+        headerTag.style.borderColor = "white";
+    } else {
+      headerTag.style.backgroundColor = "rgba(0,0,0,0)";
+      headerTag.style.borderBottom = "0px";
+      headerTag.style.borderColor = "none";
+    }
+}
+
+
+document.addEventListener("scroll", (event) => {
+  lastKnownScrollPosition = window.scrollY;
+
+  if (!ticking) {
+    // Throttle the event to "do something" every 20ms
+    setTimeout(() => {
+      setHeaderBackground(lastKnownScrollPosition);
+      ticking = false;
+    }, 20);
+
+    ticking = true;
+  }
+});
